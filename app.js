b499c2f9c0a344e3e67d51b1030c5ebe3f343043
allif <allifbrun@gmail.com>
2024-09-10 14:32:43 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,68 @@
+const USD = 4.87
+const EUR = 5.32
+const GBP = 6.08
+
+const form = document.querySelector("form");
+const amount = document.querySelector("#amount");
+const button = document.querySelector(".btn");
+const footer = document.querySelector("main footer");
+const description = document.getElementById('description');
+const result = document.getElementById("result");
+
+
+
+
+amount.addEventListener('input', ()=>{
+    const hascharacters = /\D+/g;
+    amount.value = amount.value.replace(hascharacters, '')
+
+})
+
+
+form.onsubmit = (e)=>{
+    e.preventDefault()
+    
+    switch(currency.value){
+        case "USD":
+            convertCurrency(amount.value, USD, 'US$')
+            break
+        case "EUR":
+            convertCurrency(amount.value, EUR, "€")
+            break
+        case "GBP":
+            convertCurrency(amount.value, GBP, "£")
+            break
+}
+}
+
+function convertCurrency(amount, price, symbol){
+
+    try{
+
+        description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`
+        
+        
+        let total = String(amount * price).replace(".", ",");
+
+        if(isNaN(total)){
+            return alert("Por favor, digite o valor corretamente para poder converter")
+        }
+
+        total = formatCurrencyBRL(total).replace("R$", "")
+        result.textContent = `${total} Reais`
+
+        footer.classList.add("show-result")
+
+    }catch(error){
+        console.log(error)
+        footer.classList.remove("show-result")
+        alert("Não foi possível converter. Tente novamente mais tarde.")
+    }
+}
+
+function formatCurrencyBRL(value){
+    return Number(value).toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    })
+}
